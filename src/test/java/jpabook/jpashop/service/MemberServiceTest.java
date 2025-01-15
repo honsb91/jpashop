073e27126794bc754ae1c944bbb1b8a5d3d1734a
honsb91 <114128759+honsb91@users.noreply.github.com>
2025-01-16 04:01:38 +0900

    회원가입 Test 작성 검증로직 - Test 성공

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,11 +1,13 @@
 package jpabook.jpashop.service;
 
+import jakarta.persistence.EntityManager;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,17 +24,21 @@ public class MemberServiceTest {
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    EntityManager em;
+
     @Test
     public void 회원가입() throws Exception{
 
-        //Given
+        //Given ->
         Member member = new Member();
         member.setName("na");
 
-        //When
+        //When ->
         Long saveId = memberService.join(member);
 
-        //Then
+        //Then ->
+        em.flush();
         assertEquals(member, memberRepository.findOne(saveId));
     }
 
