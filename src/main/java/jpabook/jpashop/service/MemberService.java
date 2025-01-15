a676c8bfce25217df1b1f99a380acf9b134a2179
honsb91 <114128759+honsb91@users.noreply.github.com>
2025-01-15 20:06:32 +0900

    회원가입 Service 로직작성

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    /**
+    * 회원가입
+     * */
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 전체 회원조회
+     * */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 한명 조회
+     * */
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
