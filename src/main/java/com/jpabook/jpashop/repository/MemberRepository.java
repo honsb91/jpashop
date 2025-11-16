acfacc5d2229f170b4427bb11201971738245b7a
honsb91 <114128759+honsb91@users.noreply.github.com>
2025-11-16 20:21:22 +0900

    .

---
+++ src/main/java/com/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,38 @@
+package com.jpabook.jpashop.repository;
+
+import com.jpabook.jpashop.domain.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    //회원저장
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    //회원 단건 조회
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    //회원목록
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    //회원 이름으로 검색
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name",name)
+                .getResultList();
+    }
+}
