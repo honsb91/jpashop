6baf48089704232612712b72587058a2a9fda36f
honsb91 <114128759+honsb91@users.noreply.github.com>
2025-11-21 19:24:53 +0900

    .

--- src/main/java/com/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/com/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.jpabook.jpashop.domain.item;
 
 import com.jpabook.jpashop.domain.Category;
+import com.jpabook.jpashop.exception.NotEnoughStockException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,7 +26,18 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //비즈니스 로직
+    //재고증가
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
 
-
-
+    //재고감소
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0){
+            throw new NotEnoughStockException("재고가 부족합니다!");
+        }
+        this.stockQuantity = restStock;
+    }
 }
--- src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
 class MemberServiceTest {
@@ -41,17 +40,15 @@ class MemberServiceTest {
 
         Member member2 = new Member();
         member2.setName("go");
+
         // when
         memberService.join(member1);
-        try {
-            memberService.join(member2); //예외가 발생해야 한다!!!!!
-        }catch (IllegalStateException e){
-            return;
-        }
 
-        
         // then
-        fail("예외가 발생해야 한다.");
+        // IllegalStateException 예외가 발생하지 않으면 테스트 실패
+        assertThrows(IllegalStateException.class, ()->{
+            memberService.join(member2);
+        });
+        }
     }
 
-}
